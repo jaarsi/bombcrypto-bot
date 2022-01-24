8a53a6f5ee8065982c48325f16f4719187f66a46
Jairo <jairo.araujo.silva@gmail.com>
2022-01-24 03:11:10 -0300

    Improvements

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 import os
 import asyncio
 import pyautogui as pag
+import logging
+from datetime import datetime
 
 INSTANCES_PER_COL = int(os.getenv("INSTANCES_PER_COL", 1))
 INSTANCES_PER_ROW = int(os.getenv("INSTANCES_PER_ROW", 1))
+INSTANCES_WAIT_BETWEEN_PROCCESSING = int(os.getenv("INSTANCES_WAIT_BETWEEN_PROCCESSING", 60))
 SEARCH_ASSET_MAX_TRIES = int(os.getenv("SEARCH_ASSET_MAX_TRIES", 3))
 SEARCH_ASSET_TIME_BETWEEN_TRIES = int(os.getenv("SEARCH_ASSET_TIME_BETWEEN_TRIES", 5))
 FF_REFRESH_PAGE = "images/ff_refresh_page.png"
@@ -17,6 +24,14 @@ BC_TREASURE_HUNT_CLOSE = "images/bc_treasure_hunt_close.png"
 BC_ERROR_MESSAGE = "images/bc_error_message.png"
 FF_BOMBCRYPTO = "images/ff_bombcrypto.png"
 
+logging.basicConfig(
+    filename='messages.log',
+    encoding='utf-8',
+    level=logging.INFO,
+    format="[%(asctime)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
 class EBCError(Exception):
     pass
 
@@ -24,12 +39,9 @@ async def search_and_click(region, asset, description, confidence=.9):
     tries = 0
 
     while (tries  < SEARCH_ASSET_MAX_TRIES):
-        if p := pag.locateCenterOnScreen(BC_ERROR_MESSAGE, region=region):
-            raise EBCError("Bombcrypto: Error message detected.")
-
         tries += 1
         p = pag.locateCenterOnScreen(asset, confidence=confidence, region=region)
-        print(f"{region}: Searching for '{description}' => Get position {p} #{tries}")
+        logging.info( f"{region}: Searching for '{description}' => Get position {p} #{tries}")
 
         if not p:
             await asyncio.sleep(SEARCH_ASSET_TIME_BETWEEN_TRIES)
@@ -43,9 +55,9 @@ async def process_region(region, wait_before_start):
     await search_and_click(region, FF_BOMBCRYPTO, "enter bombcrypto")
     await asyncio.sleep(30)
     await search_and_click(region, BC_CONNECT_WALLET, "connect wallet")
-    await asyncio.sleep(15)
-    await search_and_click(region, MM_SIGN_REQUEST, "sign metamask")
-    await asyncio.sleep(30)
+    await asyncio.sleep(10)
+    await search_and_click(None, MM_SIGN_REQUEST, "sign metamask")
+    await asyncio.sleep(40)
     await search_and_click(region, BC_SHOW_HEROES, "show heroes")
     await asyncio.sleep(3)
     await search_and_click(region, BC_SHOW_HEROES_WORK_ALL, "work all", .6)
@@ -66,11 +78,17 @@ async def main():
         for x in range(INSTANCES_PER_COL)
         for y in range(INSTANCES_PER_ROW)
     ]
-    jobs = [ process_region(region, i*30) for i, region in enumerate(regions, 0) ]
+    jobs = [ process_region(region, i*INSTANCES_WAIT_BETWEEN_PROCCESSING) for i, region in enumerate(regions, 0) ]
     return await asyncio.gather(*jobs)
 
 if __name__ == "__main__":
+    logging.info("[Starting]")
+
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("")
+        logging.warning("[Interrupted]")
+    except Exception as e:
+        logging.error(str(e))
+    else:
+        logging.info("[Finished]")
